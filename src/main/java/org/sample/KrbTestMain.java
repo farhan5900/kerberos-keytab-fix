ab4bbd7e82aa1de3aa7dd5f836ff3e0e676b9a5b
farhan5900 <farhan5900@gmail.com>
2019-06-25 17:20:31 +0530

    Kerberos keytab file fix

---
+++ src/main/java/org/sample/KrbTestMain.java
@@ -0,0 +1,37 @@
+package org.sample;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class KrbTestMain {
+	public static void main(String[] args) {
+		try {
+			String principal = "*";
+			String keytab = "";
+			if(args.length > 0) {
+				keytab = args[0];
+			} else {
+				System.err.println("Required atleast 1 argument, 0 provided.");
+				System.exit(1);
+			}
+			File keytabFile = new File(keytab);
+			if (!keytabFile.exists()) {
+				System.err.println("Keytab does not exists: " + keytab);
+				System.exit(1);
+			}
+			final String[] spnegoPrincipals;
+			System.out.println("Principal is: " + principal);
+			System.out.println("Keytab File is: " + keytab);
+			if (principal.equals("*")) {
+				spnegoPrincipals = KerberosUtil.getPrincipalNames(keytab, Pattern.compile("HTTP/.*"));
+			} else {
+				spnegoPrincipals = new String[]{principal};
+			}
+			System.out.println("Read Number of Principals: " + spnegoPrincipals.length);
+			System.out.println("Read First Principal: " + spnegoPrincipals[0]);
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+}
